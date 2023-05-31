5877fd8f912139e17029d8a9fca01492d818a2df
H4ck3r-x0 <h4ck3r.x0@gmail.com>
2023-05-31 12:21:17 +0300

    wip

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -29,9 +29,6 @@ class ProfileController extends Controller
     {
         $request->user()->fill($request->validated());
 
-        if ($request->user()->isDirty('email')) {
-            $request->user()->email_verified_at = null;
-        }
 
         $request->user()->save();
 
