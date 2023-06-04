920a77a9b8d2b563d2c48139f37fe388129becdb
H4ck3r-x0 <h4ck3r.x0@gmail.com>
2023-06-04 20:55:00 +0300

    fix some styles and set faker locale to arabic

--- resources/js/Components/PrimaryButton.jsx
+++ resources/js/Components/PrimaryButton.jsx
@@ -3,7 +3,7 @@ export default function PrimaryButton({ className = '', disabled, children, ...p
         <button
             {...props}
             className={
-                `inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white  hover:bg-indigo-700 focus:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 ${disabled && 'opacity-25'
+                `inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-sm text-white  hover:bg-indigo-700 focus:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 ${disabled && 'opacity-25'
                 } ` + className
             }
             disabled={disabled}
