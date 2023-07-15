24a6b256b3742addf92e5137f7ccdeef9cc5423b
H4ck3r-x0 <h4ck3r.x0@gmail.com>
2023-07-15 04:10:36 +0300

    wip

--- app/Http/Controllers/Admin/AdminDashboardController.php
+++ app/Http/Controllers/Admin/AdminDashboardController.php
@@ -13,12 +13,17 @@ class AdminDashboardController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Admin/Dashboard', [
-            'booksCount' => Book::count(),
-            'libraryCount' => Library::count(),
-            'studentCount' => User::role('user')->count(),
-            'ordersCount' => Order::count(),
-            'orderTotalPyments' => Order::sum('total_payment')
-        ]);
+
+
+        return Inertia::render(
+            'Admin/Dashboard',
+            [
+                'booksCount' => Book::count(),
+                'libraryCount' => Library::count(),
+                'studentCount' => User::role('user')->count(),
+                'ordersCount' => Order::count(),
+                'orderTotalPyments' => Order::sum('total_payment'),
+            ]
+        );
     }
 }
