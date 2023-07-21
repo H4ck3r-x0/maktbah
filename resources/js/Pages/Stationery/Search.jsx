fa1e75e1cadbdc76e0ab2932be1b3df04fae9886
H4ck3r-x0 <h4ck3r.x0@gmail.com>
2023-07-21 16:39:38 +0300

    wip on creating order for Stationery

--- resources/js/Pages/Stationery/Search.jsx
+++ resources/js/Pages/Stationery/Search.jsx
@@ -5,56 +5,56 @@ import debounce from "lodash/debounce";
 import Pagination from '@/Pages/Components/Pagination';
 import moment from 'moment/min/moment-with-locales';
 import InputLabel from '@/Components/InputLabel';
+import SecondaryButton from '@/Components/SecondaryButton';
+import PrimaryButton from '@/Components/PrimaryButton';
 
 export default function Search({ auth, note, stationeries, cities, districts }) {
-    const filters = usePage().props.filters;
-    const currentPage = usePage().props.currentPage;
+    const { props: { filters, currentPage } } = usePage();
     const [selectedCityId, setSelectedCityId] = useState('');
-    const [selectedOptions, setSelectedOptions] = useState([]);
-    const [selectedPages, setSelectedPages] = useState(0);
-    const [stationeries, setStationeries] = useState([]); // Initialize with your stationery data
+    const [selectedPages, setSelectedPages] = useState({});
+    const [selectedOptions, setSelectedOptions] = useState({});
     const { data, setData, get } = useForm({
         search: filters.search,
         city: filters.city,
         district: filters.district,
     });
 
+    const handleOptionSelect = (option, itemId) => {
+        setSelectedOptions(prevState => {
+            // If the option is already selected, remove it
+            if (prevState[itemId] && prevState[itemId].some(selectedOption => selectedOption.id === option.id)) {
+                return {
+                    ...prevState,
+                    [itemId]: prevState[itemId].filter(selectedOption => selectedOption.id !== option.id),
+                };
+            }
 
-    const handleOptionSelect = (option) => {
-        const isOptionSelected = selectedOptions.some((selectedOption) => selectedOption.id === option.id);
-
-        if (isOptionSelected) {
-            const updatedOptions = selectedOptions.filter((selectedOption) => selectedOption.id !== option.id);
-            setSelectedOptions(updatedOptions);
-        } else {
-            const updatedOptions = [...selectedOptions, option];
-            setSelectedOptions(updatedOptions);
-        }
+            // If the option is not selected, add it
+            return {
+                ...prevState,
+                [itemId]: [...prevState[itemId] || [], option],
+            };
+        });
     };
 
     const handlePagesChange = (event, itemId) => {
-        const updatedStationeries = stationeries.data.map((item) => {
-            if (item.id === itemId) {
-                return {
-                    ...item,
-                    selectedPages: parseInt(event.target.value)
-                };
-            }
-            return item;
-        });
-        // setStationeries(updatedStationeries);
+        setSelectedPages(prevState => ({
+            ...prevState,
+            [itemId]: parseInt(event.target.value)
+        }));
     };
 
-    const calculateTotalPrice = () => {
-        const totalPrice = selectedOptions.reduce((total, option) => {
-            const optionPrice = option.price * Math.ceil(selectedPages / option.per_page);
+    const calculateTotalPrice = (stationeryId) => {
+        const pages = selectedPages[stationeryId] || 0;
+        const options = selectedOptions[stationeryId] || [];
+
+        const totalPrice = options.reduce((total, option) => {
+            const optionPrice = option.price * Math.ceil(pages / option.per_page);
             return total + optionPrice;
         }, 0);
 
         return totalPrice;
     };
-
-
 
     const searchStationeries = () => {
         get(route('search.stationeries.index',
@@ -90,8 +90,8 @@ export default function Search({ auth, note, stationeries, cities, districts }) 
         setData('district', e.target.value);
     }
 
-
     return (
+
         <AuthenticatedLayout
             user={auth.user}
         >
@@ -179,9 +179,11 @@ export default function Search({ auth, note, stationeries, cities, districts }) 
                                     </div>
                                 }
                                 {stationeries.data.map(item => {
+                                    const totalPrice = calculateTotalPrice(item.id);
+
                                     return (
-                                        <div key={item.id} className='bg-white p-4 sm:p-8 rounded-md shadow-md'>
-                                            <div className='flex flex-col sm:flex-row sm:items-center gap-4 sm:gap-6'>
+                                        <div key={item.id} className='bg-white p-4 md:p-8 rounded-md shadow-md'>
+                                            <div className='flex flex-col md:flex-row md:items-center gap-4 md:gap-6'>
                                                 <h1 className='text-xl text-gray-800'>
                                                     <span className='font-semibold text-gray-700'>إسم القرطاسية </span>
                                                     <span>{item.name}</span>
@@ -195,36 +197,43 @@ export default function Search({ auth, note, stationeries, cities, districts }) 
                                                     <span>{item.district}</span>
                                                 </h1>
                                             </div>
-                                            <div className='pt-10 flex flex-col gap-4'>
+                                            <div className='pt-6 flex flex-col gap-4'>
                                                 <div className=''>
                                                     <InputLabel htmlFor="pagesInput" value={'عدد الصفحات:'} />
                                                     <input
-                                                        className='mt-2 w-full sm:max-w-sm  border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm'
-                                                        type="number" id="pagesInput" value={selectedPages || ''} onChange={(e) => handlePagesChange(e, item.id)} />
+                                                        className='mt-2 w-full md:max-w-md  border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm'
+                                                        type="number" id="pagesInput" onChange={(e) => handlePagesChange(e, item.id)} />
                                                 </div>
-
                                                 {item.printing_options.map((option) => (
-                                                    <div key={option.id} className={`flex items-center gap-3 text-xl text-gray-800 
-                                                    ${selectedOptions.some((selectedOption) => selectedOption.id === option.id) ? 'bg-blue-200' : ''}`}>
-                                                        <div className=' w-96 flex items-center gap-3 '>
+                                                    <div key={option.id} className='flex flex-col md:flex-row md:items-center gap-3 text-lg md:text-xl text-gray-800'>
+                                                        <div className='w-full md:w-96 flex items-center gap-3 '>
                                                             <span className='font-semibold text-gray-700'>{
                                                                 option.display_name} - ( {option.per_page}  صفحات )
                                                             </span>
                                                             <span>{option.price} ريال</span>
                                                         </div>
-                                                        <div className='w-36 flex itmes-center '>
-                                                            <button onClick={() => handleOptionSelect(option)} className='px-2 py-1 bg-blue-500 text-white rounded'>
-                                                                {selectedOptions.some((selectedOption) => selectedOption.id === option.id) ? 'تم الإختيار' : 'إختيار'}
-                                                            </button>
+                                                        <div className='w-full md:w-36 flex items-center '>
+                                                            <div className='w-full md:w-36 flex items-center '>
+                                                                <button onClick={() => handleOptionSelect(option, item.id)} className='px-2 py-1 bg-blue-500 text-white rounded disabled:bg-gray-400' disabled={!selectedPages[item.id]}>
+                                                                    {selectedOptions[item.id] && Array.isArray(selectedOptions[item.id]) && selectedOptions[item.id].some((selectedOption) => selectedOption.id === option.id) ? 'تم الإختيار' : 'إختيار'}
+                                                                </button>
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 ))}
-                                                <div className='text-xl text-gray-800'>إجمالي السعر: {calculateTotalPrice()} ريال</div>
-                                            </div>
 
+                                                <div className='flex items-center gap-2 mt-3 text-xl text-gray-800'>
+                                                    <span className='text-xl font-semibold text-gray-600'>إجمالي السعر</span>
+                                                    <span className='text-indigo-500'> {totalPrice} ريال</span>
+                                                </div>
+                                                <button className={`px-2 py-1 bg-blue-500 text-white rounded disabled:bg-gray-400 ${selectedPages[item.id] && selectedOptions[item.id] ? 'w-fit' : 'bg-gray-500 cursor-not-allowed w-fit'}`} disabled={totalPrice === 0 || !(selectedPages[item.id] && selectedOptions[item.id])}> إرسال الطلب </button>
+                                            </div>
                                         </div>
                                     )
                                 })}
+
+
+
                             </div>
                             <Pagination class="mt-6" links={stationeries.links} />
                         </div>
